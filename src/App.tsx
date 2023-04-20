32d5eabc275c1c35db74764b5439723e567dde8b
pouya dehghani <pouyadh.dev@gmail.com>
2023-04-21 00:29:25 +0330

    add custom scrollbar styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { CssBaseline, CssVarsProvider, Stack } from '@mui/joy';
+import { CssBaseline, CssVarsProvider, GlobalStyles, Stack } from '@mui/joy';
 import Left from './components/main/left/Left';
 import Middle from './components/main/middle/Middle';
 
@@ -6,6 +6,26 @@ function App() {
   return (
     <CssVarsProvider defaultMode="system">
       <CssBaseline />
+      <GlobalStyles
+        styles={{
+          //scrollbar webkit
+          '*::-webkit-scrollbar': {
+            width: '5px',
+          },
+          '*::-webkit-scrollbar-track': {
+            'background-color': 'transparent',
+          },
+          '*::-webkit-scrollbar-thumb': {
+            'background-color': 'rgba(128,128,128,0.3)',
+            'border-radius': '10px',
+          },
+          //scrollbar firefox
+          '*': {
+            'scrollbar-width': 'thin',
+            'scrollbar-color': '#1e1e1e transparent',
+          },
+        }}
+      />
       <Stack direction="row" spacing={0} sx={{ height: '100vh' }}>
         <Left />
         <Middle />
