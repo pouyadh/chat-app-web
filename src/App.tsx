4c2b4bdae6c9d44eb6f7bd6b6f6815115468f620
pouya dehghani <pouyadh.dev@gmail.com>
2023-04-21 01:26:24 +0330

    add background image to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,38 @@
 import { CssBaseline, CssVarsProvider, GlobalStyles, Stack } from '@mui/joy';
 import Left from './components/main/left/Left';
 import Middle from './components/main/middle/Middle';
+import bgImageDark from 'assets/chat-bg-pattern-dark.png';
+import bgImageLight from 'assets/chat-bg-pattern-light.png';
 
 function App() {
   return (
     <CssVarsProvider defaultMode="system">
       <CssBaseline />
       <GlobalStyles
-        styles={{
-          //scrollbar webkit
-          '*::-webkit-scrollbar': {
-            width: '5px',
-          },
-          '*::-webkit-scrollbar-track': {
-            'background-color': 'transparent',
-          },
-          '*::-webkit-scrollbar-thumb': {
-            'background-color': 'rgba(128,128,128,0.3)',
-            'border-radius': '10px',
-          },
-          //scrollbar firefox
-          '*': {
-            'scrollbar-width': 'thin',
-            'scrollbar-color': '#1e1e1e transparent',
-          },
+        styles={({ palette }) => {
+          const isDark = palette.colorScheme === 'dark';
+          return {
+            //scrollbar webkit
+            '*::-webkit-scrollbar': {
+              width: '5px',
+            },
+            '*::-webkit-scrollbar-track': {
+              backgroundColor: 'transparent',
+            },
+            '*::-webkit-scrollbar-thumb': {
+              backgroundColor: 'rgba(128,128,128,0.3)',
+              borderRadius: '10px',
+            },
+            //scrollbar firefox
+            '*': {
+              scrollbarWidth: 'thin',
+              scrollbarColor: '#1e1e1e transparent',
+            },
+            body: {
+              backgroundImage: `url(${isDark ? bgImageDark : bgImageLight})`,
+              backgroundSize: 'contain',
+            },
+          };
         }}
       />
       <Stack direction="row" spacing={0} sx={{ height: '100vh' }}>
